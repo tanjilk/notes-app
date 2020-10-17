f1dc789791db69d67a98c96a9351d3aeed10b25c
tanjilk <tanjilkh@gmail.com>
2020-10-17 23:49:49 +0100

    added the body argument

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
+// Get the required npm modules
+
 const chalk = require('chalk');
-const { demand } = require('yargs');
 const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
+// Create the add command
 yargs.command({
     command: 'add',
+
+    // Description of the command
+
     describe: 'Add a new note',
+
+    // Additional arguments
     builder:{
+        // Add title argument
+
         title: {
             describe: 'Note title',
             demandOption : true,
             type: 'string'
+        },
+        // Add body argument
+
+        body: {
+            describe: 'The content of the note',
+            demandOption: true,
+            type: 'string'
         }
     },
     handler : function(argv){
-        console.log('Adding a new note...');
-        console.log(chalk.green(argv.title + ' succesful added!'));
+        console.log(chalk.yellow('Informations about the note...'));
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
     }
 })
 
+// Create the remove command
 yargs.command({
     command: 'remove',
     describe: 'Remove an note',
